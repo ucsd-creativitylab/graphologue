90e3cdce6ca50e5c98a186b10471978ea4758b7d
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2023-02-25 22:57:37 -0600

    minor

--- src/utils/openAI.ts
+++ src/utils/openAI.ts
@@ -11,7 +11,7 @@ export const getCompletionOptions = (prompt: string) => ({
   ////
   model: 'text-davinci-003',
   temperature: 0.7,
-  max_tokens: 500,
+  max_tokens: 512,
   top_p: 1,
   frequency_penalty: 0,
   presence_penalty: 0,
