c8cb21f624b450bdf84571b7c370d0ae6f5e9dde
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2023-03-27 00:07:25 -0700

    update

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -170,7 +170,7 @@ export const Question = ({
         initialPrompts,
         answerStorage.current.answer
       ),
-      models.faster
+      models.smarter
     )
     if (brokenResponseData.error) return handleResponseError(brokenResponseData)
 
