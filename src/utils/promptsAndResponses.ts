01c60b4dccaaeb4d50e4301ef62dea01a7735471
Peiling Jiang <25191575+peilingjiang@users.noreply.github.com>
2023-04-02 01:55:22 -0700

    update prompt

--- src/utils/promptsAndResponses.ts
+++ src/utils/promptsAndResponses.ts
@@ -72,9 +72,11 @@ Entities are usually noun phrases and should be annotated with [entity ($N1)], f
 Do not annotate conjunctive adverbs, such as "since then" or "therefore," as entities in the map. \
 \
 A relationship is usually a word or a phrase that consists of verbs, adjectives, adverbs, or propositions. \
-Relationships should be annotated with the relevant entities and saliency of the relationship as high ($H) or low ($L), in the format of [relationship ($H, $N1, $N2)], \
+Relationships should be annotated with the relevant entities and saliency of the relationship, as high ($H) or low ($L), in the format of [relationship ($H, $N1, $N2)], \
 for example, [AI systems ($N1)] can be [divided into ($H, $N1, $N9; $H, $N1, $N10)] [narrow AI ($N9)] and [general AI ($N10)]. \
 Relationships of high saliency are often included in summaries. Relationships of low saliency are often omitted in summaries. \
+It's important to choose relationships that accurately reflect the nature of the connection between the concepts in text, \
+and to use consistent labeling conventions throughout the paragraphs. \
 \
 You should try to annotate at least one relationship for each entity. Relationships should only connect entities that appear in the response.
 
